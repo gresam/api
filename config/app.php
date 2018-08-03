20c9b5b5f73915b7ed644c2230fde889817488a5
gresa <gresa.mecaj@gmail.com>
2018-08-03 10:53:07 +0200

    Relationship created

--- config/app.php
+++ config/app.php
@@ -163,6 +163,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Laravel\Tinker\TinkerServiceProvider::class,
+
 
         /*
          * Package Service Providers...
