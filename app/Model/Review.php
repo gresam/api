b7eef1d0ac5894ab30da6631bfa1df0671e3dfec
gresa <gresa.mecaj@gmail.com>
2018-08-20 16:26:35 +0200

    Review Crated

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+
+    protected  $fillable =  [
+
+        'star','customer','review'
+    ];
     public function products(){
 
         return $this->belongsTo(Product::class);
